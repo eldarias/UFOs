79f3c3b2152fe8292dc06e77c64836f64e853cb8
Elder Arias <Elder.Arias@live.com>
2022-03-14 01:34:41 -0400

    Challenge 11 - Submission

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # UFOs
-Module 11 -  JavaScript, Bootstrap, and UFOs
+Module 11: JavaScript, Bootstrap, and UFOs
+
+## Overview of Project
+The purpose of this challenge was to update the JavaScript code and HTML file to add additional filters.  In addition to the **date** filter, the **city**, **state**, **country** and **shape** filters were added.  The additional filters allow users to search for UFO sightings using multiple criteria.
+
+
+## Results
+Below are the filters available that users can now use to search for UFO sightings from the available data.
+- [<image src="./Results/AvailableFilters.PNG" width="200" height="200">](https://github.com/eldarias/UFOs/blob/main/Results/AvailableFilters.PNG)<b></b>
+
+
+### Using Filter Search: 
+Here are some examples of how users can use the filters to query for only the UFO sightings of interest:
+
+- Example Search using Date and City where the **Date** = _January 12 2010 (1/12/2010)_ and **city** = _wichita_
+    - [<image src="./Results/SearchExample01.PNG" width="800" height="600">](https://github.com/eldarias/UFOs/blob/main/Results/SearchExample01.PNG)<b></b>
+- Example Search using State where the **State** = _ny_
+    - [<image src="./Results/SearchExample02.PNG" width="800" height="600">](https://github.com/eldarias/UFOs/blob/main/Results/SearchExample02.PNG)<b></b>
+
+
+## Summary
+In summary, the new JavaScript code and HTML file accomplish the goal of allowing users to filter UFO sightings using multiple criteria, but there are improvements that can be made.
+The limitations observed or recommended improvements to the **Filter Search** are as follows:
+- The filters are case-sensitive therefore data will be returned only if users enter lowercase text.
+- The date field does not work if 0 is entered in front of months and days, for example: January 1, 2010 must be entered as 1/1/2010, if entering 01/01/2010, it will not be recognized.
+- The Filter Search can also be improved by adding multiple criteria for the same filter, for example: State = NY or State = CA.
--- static/js/app.js
+++ static/js/app.js
@@ -4,48 +4,77 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// Fuction:
+// This function will be used to fill the table to display UFO sightings data
 function buildTable(data) {
-  // First, clear out any existing data
+  // First, clear out any existing data from the table body in the HTML Page:
   tbody.html("");
 
-  // Next, loop through each object in the data
+  // Next, loop through each object in the data using forEach and fat arrow:
   // and append a row and cells for each value in the row
   data.forEach((dataRow) => {
-    // Append a row to the table body
+    // Append a row to the table body using "let", so the variable row is limited to just this block of code:
+    // the below tells JS to find the <tbody> tag within HTML and add a table row:
     let row = tbody.append("tr");
 
     // Loop through each field in the dataRow and add
     // each value as a table cell (td)
+    // object.values tells TV to referecen one object from the array of UFO Sightings.  the (dataRow) argment says we want the values to go into the dataRow.  foEach((val)) specifies we want one object per row
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
-      }
-    );
+    });
   });
 }
 
-function handleClick() {
-  // Grab the datetime value from the filter
-  let date = d3.select("#datetime").property("value");
-  let filteredData = tableData;
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
 
-   // Check to see if a date was entered and filter the
-  // data using that date.
-  if (date) {
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
-    filteredData = filteredData.filter(row => row.datetime === date);
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+    let changedElement = d3.select(this);
+
+    // 4b. Save the value that was changed as a variable.
+    let changedElementProperty = changedElement.property("value");
+
+    // 4c. Save the id of the filter that was changed as a variable.
+    let changedElementId = changedElement.attr("id")
+
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (changedElementProperty) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filters[changedElementId] = changedElementProperty;
+    }
+    else {
+      delete filters[changedElementId];
+    }
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
   }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.  Our default filter will actually be the original table data because we want users to refine their search on their own terms.
+    let filteredData = tableData
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values.  "show only the rows equal to the filters data"
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+  
+    // 10. Finally, rebuild the table using the filtered data
+    buildTable(filteredData)
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
 
-   // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
-  buildTable(filteredData);
-}
-
-// Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
-
-// Build the table when the page loads
-buildTable(tableData);
+  // Build the table when the page loads
+  buildTable(tableData);
